49ad0f5f5e50c28006d908ba8b7f071231b42e05
ginold <ginold@hotmail.com>
2020-10-06 22:12:06 +0200

    ignore yellow box console warnings

--- App.tsx
+++ App.tsx
@@ -5,15 +5,8 @@ import configureStore from './configureStore'
 import { Provider } from 'react-redux';
 import AppProvider from './ApplicationProvider'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { YellowBox } from "react-native";
 
-console.ignoredYellowBox = ['Setting a timer:'];
-YellowBox.ignoreWarnings(["Setting a timer"]);
-// console.warn = message => {
-//   if (message.indexOf('Setting a timer') <= -1) {
-//     console.warn(message);
-//   }
-// };
+console.disableYellowBox = true;
 
 export const myStore = configureStore()
 
